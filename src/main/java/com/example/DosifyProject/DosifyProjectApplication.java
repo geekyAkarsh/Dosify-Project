40b310cba1aa7a7082e77ba74163cf0d230de7ef
Akarsh <akarshsingh81@gmail.com>
2023-05-24 22:35:12 +0530

    first commit

---
+++ src/main/java/com/example/DosifyProject/DosifyProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.DosifyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosifyProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DosifyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DosifyProject/DosifyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DosifyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosifyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
