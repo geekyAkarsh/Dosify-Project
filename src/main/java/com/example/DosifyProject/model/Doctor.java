f5ba135b4ea70ef6f32422a63cd22b3d121c3845
Akarsh <akarshsingh81@gmail.com>
2023-05-27 11:07:50 +0530

    added doctor model class

---
+++ src/main/java/com/example/DosifyProject/model/Doctor.java
@@ -0,0 +1,37 @@
+package com.example.DosifyProject.model;
+
+import com.example.DosifyProject.Enum.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "doctor")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "age")
+    Integer age;
+
+    @Column(name = "email_id",unique = true,nullable = false)
+    String emailId;
+
+    @Column(name = "mob_no",unique = true,nullable = false)
+    String mobNo;
+
+    @Enumerated
+    @Column(name = "gender")
+    Gender gender;
+
+}
