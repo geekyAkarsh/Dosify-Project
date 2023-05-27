549efcafb22e5476823c9444dacc89e45c5ddd39
Akarsh <akarshsingh81@gmail.com>
2023-05-27 11:13:01 +0530

    added vaccinationCenter model class

---
+++ src/main/java/com/example/DosifyProject/Enum/CenterType.java
@@ -0,0 +1,10 @@
+package com.example.DosifyProject.Enum;
+
+public enum CenterType {
+
+    PRIVATE_HOSPITAL,
+    GOVERNMENT_HOSPITAL,
+    SCHOOL,
+    NGO,
+    OTHER
+}
---
+++ src/main/java/com/example/DosifyProject/model/VaccinationCenter.java
@@ -0,0 +1,32 @@
+package com.example.DosifyProject.model;
+
+import com.example.DosifyProject.Enum.CenterType;
+import com.example.DosifyProject.Enum.VaccineType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "Center")
+public class VaccinationCenter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "location")
+    String location;
+
+    @Enumerated
+    @Column(name = "center_type")
+    CenterType centerType;
+
+}
