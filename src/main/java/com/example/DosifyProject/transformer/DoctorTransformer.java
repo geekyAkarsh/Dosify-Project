44353dec7da43dea190dd93571906784e07c78b0
Akarsh <akarshsingh81@gmail.com>
2023-06-09 23:34:13 +0530

    completed doctor transformer

--- src/main/java/com/example/DosifyProject/transformer/DoctorTransformer.java
+++ src/main/java/com/example/DosifyProject/transformer/DoctorTransformer.java
@@ -1,6 +1,8 @@
 package com.example.DosifyProject.transformer;
 
 import com.example.DosifyProject.dto.RequestDTO.DoctorRequestDto;
+import com.example.DosifyProject.dto.ResponseDTO.CenterResponseDto;
+import com.example.DosifyProject.dto.ResponseDTO.DoctorResponseDto;
 import com.example.DosifyProject.model.Doctor;
 import com.example.DosifyProject.model.VaccinationCenter;
 import com.example.DosifyProject.service.CenterService;
@@ -10,8 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @UtilityClass
 public class DoctorTransformer {
 
-    @Autowired
-    CenterService centerService;
+
     public static Doctor DoctorRequestDtoToDoctor(DoctorRequestDto doctorRequestDto){
 
         return Doctor.builder()
@@ -20,9 +21,20 @@ public class DoctorTransformer {
                 .mobNo(doctorRequestDto.getMobNo())
                 .emailId(doctorRequestDto.getEmailId())
                 .gender(doctorRequestDto.getGender())
-                .vaccinationCenter(centerService.)
                 .build();
-
     }
+
+    public static DoctorResponseDto DoctorToDoctorResponseDto(Doctor doctor){
+
+        CenterResponseDto centerResponseDto = CenterTransformer.VaccinationCenterToCenterResponseDto(doctor.getVaccinationCenter());
+
+        return DoctorResponseDto.builder()
+                .name(doctor.getName())
+                .emailId(doctor.getEmailId())
+                .mobNo(doctor.getMobNo())
+                .centerResponseDto(centerResponseDto)
+                .build();
+    }
+
 
 }
